03268490b2daf6fb725752e630769249babf6b13
Docen89 <docen89@ya.ru>
2024-09-19 08:49:39 +0500

    v2

--- src/test/java/api/JsonArray.java
+++ src/test/java/api/JsonArray.java
@@ -5,17 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class JsonArray {
+
   private String userId;
   private List<ArrayItem> collectionOfIsbns;
+
   @Data
   @NoArgsConstructor
   @AllArgsConstructor
   @Builder
-  public static class ArrayItem{
+  public static class ArrayItem {
+
     private String isbn;
   }
 }
--- src/test/java/test/TestBookMarket.java
+++ src/test/java/test/TestBookMarket.java
@@ -1,12 +1,15 @@
 package test;
 
+import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.switchTo;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 import api.ApiGetToken;
 import api.ApiLogin;
 import api.JsonArray;
 import api.JsonArray.ArrayItem;
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,40 +19,42 @@ import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import java.io.File;
 import java.util.List;
-import lombok.SneakyThrows;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.Cookie;
+import org.testng.Assert;
 
 
 public class TestBookMarket {
+
   private config cfg = ConfigFactory.create(config.class);
   public ApiLogin apiLogin;
   public ApiGetToken apiGetToken;
 
   @BeforeEach
-  public   void setup(){
+  public void setup() {
     apiLogin = new ApiLogin();
     apiGetToken = new ApiGetToken();
-    RestAssured.baseURI=cfg.baseUri();
-    SelenideLogger.addListener("allure", new AllureSelenide());
+    RestAssured.baseURI = cfg.baseUri();
+    Configuration.pageLoadStrategy = cfg.pageLoadStrategy();
+    Configuration.browserSize = cfg.browserSize();
   }
 
   @Test
+  @DisplayName("Добавляем книгу в коллекцию")
   @Step("Добавляем книгу в коллекцию")
-  @DisplayName("1")
-  public void addBooK(){
-    String userId=apiLogin.getUserIdValue();
-    JsonArray jsonArray =new JsonArray();
+  public void addBooK() {
+    String userId = apiLogin.getUserIdValue();
+    JsonArray jsonArray = new JsonArray();
     jsonArray.setUserId(userId);
     jsonArray.setCollectionOfIsbns(List.of(ArrayItem.builder().isbn("9781449325862").build()));
     ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-         RestAssured.given()
-        .auth().preemptive().basic(cfg.userName(),cfg.password())
+    RestAssured.given()
+        .auth().preemptive().basic(cfg.userName(), cfg.password())
         .contentType(ContentType.JSON)
         .body(jsonArray)
         .log().all()
@@ -60,21 +65,30 @@ public class TestBookMarket {
 
   @Test
   @Step("Удаляем книгу из коллекции")
-  @DisplayName("2")
-  public  void  deleteBool(){
-    String userId=apiLogin.getUserIdValue();
-    String  token =apiGetToken.getTokenValue();
-    String expires =apiGetToken.getExpiresValue();
-    System.out.println(userId);
-    System.out.println(token);
-    System.out.println(expires);
+  @DisplayName("Удаляем книгу из коллекции")
+  public void deleteBool() {
+    SelenideLogger.addListener("allure", new AllureSelenide());
+    String userId = apiLogin.getUserIdValue();
+    String token = apiGetToken.getTokenValue();
+    String expires = apiGetToken.getExpiresValue();
     open(cfg.baseUri());
-    WebDriverRunner.getWebDriver().manage().addCookie(new Cookie("userID",userId));
-    WebDriverRunner.getWebDriver().manage().addCookie(new Cookie("token",token));
-    WebDriverRunner.getWebDriver().manage().addCookie(new Cookie("expires",expires));
-    open(cfg.profilePath());
-    Selenide.sleep(30000);
+    WebDriverRunner.getWebDriver().manage().addCookie(new Cookie("userID", userId));
+    WebDriverRunner.getWebDriver().manage().addCookie(new Cookie("token", token));
+    WebDriverRunner.getWebDriver().manage().addCookie(new Cookie("expires", expires));
+    step("Открываем авторизованную под пользователе страницу" + cfg.profilePath(), () -> {
+      open(cfg.profilePath());
+    });
+    step("Кликаем по кнопку 'удалить'", () -> {
+      $x("//span[@id='delete-record-undefined']").click();
+    });
+    step("Потверждаем удаление книги", () -> {
+      $x("//button[@id='closeSmallModal-ok']").click();
+    });
+    step("Получаем сообщение об удаление книги", () -> {
+      Alert jsal = switchTo().alert();
+      String jsal_text = jsal.getText();
+      System.out.println(jsal_text);
+      Assert.assertEquals(jsal_text, "Book deleted.");
+    });
   }
-
-
 }
