a1ef743a4bd2881370c04343fb2ada5a1fc6b1da
Docen89 <docen89@ya.ru>
2024-10-04 14:14:32 +0500

    v3

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,15 +1,17 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Screenshots;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.google.common.io.Files;
-import io.qameta.allure.Allure;
+
+import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.IOException;
+
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -21,17 +23,33 @@ public class BaseTest {
 
   @BeforeAll
   public static void setup() {
-    String browserSize = System.getProperty("browserSize");
-    String baseUrl = System.getProperty("baseURI");
-    String pageLoadStrategy = System.getProperty("pageLoadStrategy");
+    String browserSize =
+        System.getProperty("browserSize");
+    String baseUrl =
+        System.getProperty("baseURI");
+    String pageLoadStrategy =
+        System.getProperty("pageLoadStrategy");
     Configuration.pageLoadStrategy =
         pageLoadStrategy != null ? pageLoadStrategy : cfg.pageLoadStrategy();
     Configuration.browserSize =
         browserSize != null ? browserSize : cfg.browserSize();
     RestAssured.baseURI =
         baseUrl != null ? baseUrl : cfg.baseUri();
-    SelenideLogger.addListener("allure", new AllureSelenide());
+    SelenideLogger.addListener("allureSelenide",
+        new AllureSelenide().screenshots(true).savePageSource(true));
+
   }
 
+  @AfterEach
+  public void tearDown() throws IOException {
+    screenshot();
+  }
 
+  @Attachment(type = "image/png")
+  public byte[] screenshot() throws IOException {
+    File screenshot = Screenshots.getLastScreenshot();
+    return screenshot == null ? null : Files.toByteArray(screenshot);
+  }
 }
+
+
